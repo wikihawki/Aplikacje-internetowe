095bfbed2253cff69808d6a54746e4707b212e67
lisek2023 <lisek02023@gmail.com>
2016-11-24 17:48:13 +0100

    Wygenerowany Projekt

---
+++ Ogloszenia Studenckie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ogloszenia_Studenckie.Startup))]
+namespace Ogloszenia_Studenckie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
